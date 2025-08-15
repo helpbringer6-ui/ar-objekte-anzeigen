d9933d9e7ac5d7dde283ae0532c1475e85ffd95e
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-14 23:39:11 -0500

    feat: initial commit for src/Scripts/ModelManager.cs

---
+++ src/Scripts/ModelManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ModelManager : MonoBehaviour
+{
+    public GameObject[] furnitureModels;
+
+    public void LoadModel(int index)
+    {
+        // Load the selected furniture model into the scene
+    }
+
+    public void AdjustModelSize(float newSize)
+    {
+        // Adjust the size of the placed model
+    }
+}
